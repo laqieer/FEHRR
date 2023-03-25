a37d332b0ab4f6b924aef3f3f708d194cdb3ef0e
laqieer <laqieer@126.com>
2023-03-26 02:30:20 +0800

    Fix voice missing in some cases
    Adjust voice priority from lowest to highest to ensure that it always plays on any occasion. Vanilla music player with highest priority is for battle BGM, so it is required to disable battle BGM and continue map BGM during battle.

---
+++ source/banim.c
@@ -0,0 +1,13 @@
+
+void PlayBattleMusic(int a1, int a2, int a3);
+void RestoreMapMusicAfterBattle(int a1);
+
+void PlayBattleMusicOld(int a1, int a2, int a3)
+{
+
+}
+
+void RestoreMapMusicAfterBattleOld(int a1)
+{
+
+}
