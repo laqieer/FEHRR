a33b835c3159c48ba08ab10a9aaea4d24b4fb35e
laqieer <laqieer@126.com>
2023-03-29 21:09:03 +0800

    Show hidden options in configuration menu

---
+++ source/optionmenu.c
@@ -0,0 +1,7 @@
+#include <gba_types.h>
+
+const u8 OptionsMenuItems[] = {0, 5, 4, 1, 2, 10, 11, 3, 12, 6, 7, 8, 9, 13};
+
+const int OptionsMenuItemNum = sizeof(OptionsMenuItems)/sizeof(OptionsMenuItems[0]);
+
+const u8 * const pOptionsMenuItems = OptionsMenuItems;
