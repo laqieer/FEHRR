a4ebd258f47fe1d3d7ddb5075d257a2b8aaecc08
laqieer <laqieer@126.com>
2024-06-22 20:05:57 +0800

    Fix script run order when update assets
    make_heroes should be after make_voices

--- update-assets.bat
+++ update-assets.bat
@@ -1,9 +1,9 @@
 git submodule update --remote asset/json
 python3 -m pip install pillow opencc
 python3 asset/script/make_backgrounds.py
-python3 asset/script/make_faces.py
 python3 asset/script/make_voices.py
 python3 asset/script/make_heroes.py
+python3 asset/script/make_faces.py
 python3 asset/script/make_texts.py
 python3 glyph/glyph.py
 pause
--- update-assets.sh
+++ update-assets.sh
@@ -3,8 +3,8 @@
 git submodule update --remote asset/json
 pip install pillow opencc
 asset/script/make_backgrounds.py
-asset/script/make_faces.py
 asset/script/make_voices.py
 asset/script/make_heroes.py
+asset/script/make_faces.py
 asset/script/make_texts.py
 glyph/glyph.py
