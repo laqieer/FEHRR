d12573f10d3fdaec6909065ad2ca3fb1221bb7e9
laqieer <laqieer@126.com>
2024-07-02 14:41:34 +0800

    Fix number display issue
    Fix NumberToString to convert number to string encoded in UTF-8 instead of Shift-JIS

--- source/utilNew.c
+++ source/utilNew.c
@@ -57,3 +57,66 @@ void SetBlendTargetBNew(int bg0, int bg1, int bg2, int bg3, int obj)
     gDispIo.blend_ct.target2_enable_bg3 = bg3;
     gDispIo.blend_ct.target2_enable_obj = obj;
 }
+
+const char sStrZeroUtf8[] = "０";
+const char sStrDashUtf8[] = "ー";
+
+int NumberToStringNew(int number, char * buf)
+{
+    int numOff, numStart;
+
+    numOff = 0;
+
+    if (number == 0)
+    {
+        *buf++ = sStrZeroUtf8[0];
+        *buf++ = sStrZeroUtf8[1];
+        *buf++ = sStrZeroUtf8[2];
+        *buf++ = '\0';
+
+        return 1;
+    }
+
+    if (number < 0)
+    {
+        buf[0] = sStrDashUtf8[0];
+        buf[1] = sStrDashUtf8[1];
+        buf[2] = sStrDashUtf8[2];
+
+        number = -number;
+        numOff = 3;
+    }
+
+    if (number > 99999)
+        numOff += 15;
+    else if (number > 9999)
+        numOff += 12;
+    else if (number > 999)
+        numOff += 9;
+    else if (number > 99)
+        numOff += 6;
+    else if (number > 9)
+        numOff += 3;
+
+    numStart = numOff;
+
+    while (number > 0)
+    {
+        int rem = DivRem(number, 10);
+
+        buf[numOff]   = sStrZeroUtf8[0];
+        buf[numOff+1] = sStrZeroUtf8[1];
+        buf[numOff+2] = sStrZeroUtf8[2] + rem;
+
+        number = Div(number, 10);
+        numOff -= 3;
+    }
+
+    *(buf + numStart + 3) = '\0';
+    return (numStart / 3) + 1;
+}
+
+int NumberToStringOld(int number, char * buf)
+{
+    return NumberToStringNew(number, buf);
+}
