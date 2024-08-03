be0c8e09277ad844c2560a9df79d6aa149592bfe
laqieer <laqieer@126.com>
2024-08-03 16:09:50 +0800

    Add a script to find the best transcribed midi

---
+++ music/trans/match.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# This script is expected to be run in music/trans/ folder
+
+import os
+import json
+from midi2audio import FluidSynth
+from audio_similarity.audio_similarity.audio_similarity import AudioSimilarity
+
+sample_rate = 44100
+weights = {
+    'zcr_similarity': 0.2,
+    'rhythm_similarity': 0.2,
+    'chroma_similarity': 0.2,
+    'spectral_contrast_similarity': 0.1,
+    'perceptual_similarity': 0.2
+}
+fs = FluidSynth('../../instrument/FE6 native instrument map/FE6 soundfont.sf2', sample_rate=sample_rate)
+
+def calculate_match_score(original_audio_file, midi_file):
+    fs.midi_to_audio(midi_file, 'temp.wav')
+    audio_similarity = AudioSimilarity('../original/' + original_audio_file, 'temp.wav', sample_rate, weights)
+    match_score = {
+        'zcr_similarity': audio_similarity.zcr_similarity(),
+        'rhythm_similarity': audio_similarity.rhythm_similarity(),
+        'chroma_similarity': audio_similarity.chroma_similarity(),
+        'spectral_contrast_similarity': audio_similarity.spectral_contrast_similarity(),
+        'perceptual_similarity': audio_similarity.perceptual_similarity(),
+        'stent_weighted_audio_similarity': audio_similarity.stent_weighted_audio_similarity(metrics='all')
+    }
+    os.remove('temp.wav')
+    return match_score
+
+match_scores = {}
+
+for original_audio_file in os.listdir('../original/'):
+    if original_audio_file.endswith('.ogg'):
+        match_scores[original_audio_file] = {}
+
+for root, dirs, files in os.walk('.'):
+    for file in files:
+        if file.endswith('.mid'):
+            original_audio_file = file.replace('.mid', '.ogg')
+            file = os.path.join(root, file)
+            match_scores[original_audio_file][file] = calculate_match_score(original_audio_file, file)
+
+# Save match scores to a json file
+with open('match_scores.json', 'w') as f:
+    json.dump(match_scores, f, indent=4)
+
+match_results = {}
+# Find best match for each original audio file
+for original_audio_file in match_scores:
+    best_match = None
+    best_match_score = 0
+    for midi_file in match_scores[original_audio_file]:
+        match_score = match_scores[original_audio_file][midi_file]['stent_weighted_audio_similarity']
+        if match_score > best_match_score:
+            best_match = midi_file
+            best_match_score = match_score
+    match_results[original_audio_file] = {
+        'best_match': best_match,
+        'best_match_score': best_match_score
+    }
+
+# Save match results to a json file
+with open('match_results.json', 'w') as f:
+    json.dump(match_results, f, indent=4)
