e3c4ad5d5eb2d327ebd8c16c3d0f592dbee64937
laqieer <laqieer@126.com>
2024-08-20 20:46:25 +0800

    Fix ROM size calculation script
    - try to build target ROM if it doesn't exist
    - fix stat format arg for macOS based on BSD

--- check_rom_size.sh
+++ check_rom_size.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+if [ ! -f "FEHRR.gba" ]; then
+  echo "Info: FEHRR.gba not found, trying to build it"
+  make
+fi
+
+if [ ! -f "FEHRR.gba" ]; then
+  echo "Error: failed to build FEHRR.gba"
+  exit 1
+fi
+
 # Get the file size in bytes
-filesize=$(stat -c%s "FEHRR.gba")
+filesize=$(stat -c%s "FEHRR.gba") # GNU stat
+if [ -z "$filesize" ]; then
+  filesize=$(stat -f%z "FEHRR.gba") # BSD stat
+fi
+
+# Fail to get the file size
+if [ -z "$filesize" ]; then
+  echo "Error: failed to get the file size"
+  exit 2
+fi
 
 # Define the maximum allowed size (32MB)
 max_size=$((32 * 1024 * 1024))
@@ -14,7 +33,7 @@ echo "Info: current size is $filesize bytes, ratio is $ratio%"
 # Check if the file size exceeds the maximum allowed size
 if [ "$filesize" -gt "$max_size" ]; then
   echo "Error: FEHRR.gba exceeds 32MB"
-  exit 1
+  exit 3
 fi
 
 echo "FEHRR.gba is within the allowed size"
