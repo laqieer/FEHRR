f4b5654919c3c13da041c4b5c02efb83aa724e74
laqieer <laqieer@126.com>
2024-08-25 11:04:41 +0800

    Fix check_baserom.sh in macOS
    ```
    ./check_baserom.sh: line 16: ${actual_sha1,,}: bad substitution
    ```
    
    Change `expected_sha1` to lower case

--- check_baserom.sh
+++ check_baserom.sh
@@ -7,13 +7,13 @@ if [ ! -f baserom.gba ]; then
 fi
 
 # Define the expected SHA-1 value
-expected_sha1="A57095DA867DE4D585C33D4394712986245FE6CA"
+expected_sha1="a57095da867de4d585c33d4394712986245fe6ca"
 
 # Calculate the SHA-1 of the file (use shasum because sha1sum is not available on macOS)
 actual_sha1=$(shasum baserom.gba | awk '{print $1}')
 
 # Compare the calculated SHA-1 with the expected value
-if [ "${actual_sha1,,}" = "${expected_sha1,,}" ]; then
+if [ "${actual_sha1}" = "${expected_sha1}" ]; then
     echo "SHA-1 matches!"
     exit 0
 else
